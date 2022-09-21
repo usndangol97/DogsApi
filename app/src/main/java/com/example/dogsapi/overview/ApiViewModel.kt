35a6264b1540896619d3ba11cb360cc5980943f4
usndangol97 <usndangol97@gmail.com>
2022-09-21 15:39:10 +0545

    Made changes to process api calls

--- app/src/main/java/com/example/dogsapi/overview/ApiViewModel.kt
+++ app/src/main/java/com/example/dogsapi/overview/ApiViewModel.kt
@@ -1,21 +1,27 @@
 package com.example.dogsapi.overview
 
+import android.content.Context
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.dogsapi.api.DogsInfoApi
 import com.example.dogsapi.data.ApiDataClass
+import com.squareup.moshi.JsonDataException
 import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import kotlin.math.log
 
 class ApiViewModel: ViewModel() {
 //    val xApiKey = "live_YDrP3rk8ukEVjAjVGS13jbOrpt8hUs0Jok4Ib9Xr5RR9qJl9JOJtjv6uJqUr46vx"
-    private val _status = MutableLiveData<ApiDataClass>()
+    private val _status = MutableLiveData<String>()
 
     // The external immutable LiveData for the request status
-    val status: LiveData<ApiDataClass> = _status
+    val status: LiveData<String> = _status
 
     init {
         getInfoDogs()
@@ -23,13 +29,36 @@ class ApiViewModel: ViewModel() {
 
     private fun getInfoDogs() {
         viewModelScope.launch{
+//            DogsInfoApi.retrofitService.getInfoDogs().enqueue(object: Callback<List<ApiDataClass>> {
+//                override fun onResponse(
+//                    call: Call<List<ApiDataClass>>,
+//                    response: Response<List<ApiDataClass>>
+//                ) {
+//                    if (response.code() == 200) {
+//                        _status.value = "Success: ${response.body()?.size ?: 0} Dogs Info retrieved"
+//                    } else {
+//                        _status.value = response.errorBody()?.toString() ?: "Something went wrong."
+//                    }
+//                }
+//
+//                override fun onFailure(call: Call<List<ApiDataClass>>, t: Throwable) {
+//                    _status.value = t.message
+//                }
+//            })
+
             try {
                 val listRes = DogsInfoApi.retrofitService.getInfoDogs()
-                Log.d("Result","@{listRes}")
-                _status.value = listRes
+                _status.value = "Success: ${listRes.size} Dogs Info retrieved"
+            }catch (e: JsonDataException){
+                Log.d(TAG, "getInfoDogs: ${e.toString()}")
+                _status.value = "Json Data Exception"
             }catch (e: Exception){
-//                _status.value = e.message
+                _status.value = e.message
             }
         }
+    }
+
+    companion object {
+        private const val TAG ="ApiViewModel"
     }
 }
